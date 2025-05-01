c799bea0969c350080c3abf729c84d38a31d67a6
Lester del Rosario <79028758+Lester1105@users.noreply.github.com>
2025-05-01 22:11:14 +0800

    commit change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      serverActions: true, // Only if you're using Server Actions in the App Router
+    },
+  };
+  
+  export default nextConfig;
